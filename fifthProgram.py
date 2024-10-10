84afb803c35b11f30cfe412bb681ac9910c1329b
Umama khan <umamakhan427@gmail.com>
2024-10-10 15:10:17 +0500

    # DAY 5:
    Topic:
    If-elif-else
    days passes my excitement increases

---
+++ fifthProgram.py
@@ -0,0 +1,7 @@
+#Day5: 27 sep 2024
+width=int(input("Enter the width of the rectangle:"))
+height=int(input("Enter the height of the rectangle:"))
+area = width*height
+perimeter=2*(width+height)
+print(f"Area of the rectangle,{area}")
+print(f"the perimeter of the rectangle,{perimeter}")
