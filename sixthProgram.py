e67836cf53939c03b1a3c3ab1f1be799706c90ff
Umama khan <umamakhan427@gmail.com>
2024-10-11 15:15:09 +0500

    #Day 6
    Here I learn nested if and boolean logic 
    it's amazing 
    my sir also give me task so I done the task

---
+++ sixthProgram.py
@@ -0,0 +1,14 @@
+#Day6: 28 sep 2024
+#condition use
+age = 20
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+# even odd 
+number = int(input("Enter the number:"))
+if number%2==0:
+    print("Even")
+else:
+    print("Odd")
