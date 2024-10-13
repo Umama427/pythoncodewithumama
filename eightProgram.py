5de8076cf3f54467726fc7865fefa055eeb5385c
Umama khan <umamakhan427@gmail.com>
2024-10-13 13:46:55 +0500

    Day 8:
    This Python code uses the match statement to respond to user input:
    
    User Input: It asks the user to enter a number, which is stored in x.
    Case Handling:
    If x is 0, 1, 2, or 3, it prints a specific message for each.
    For other numbers, it checks if x is not 90 or 80 and gives feedback.
    If none of these cases match, it just prints the value of x.

---
+++ eightProgram.py
@@ -0,0 +1,11 @@
+#Day8: 30 sep 2024
+#for loop -> sequence numbers from 1 to 10 skip 11
+for i in range(1,11):
+    print(i)
+
+#while loop -> when condition meet true so the loop stops:
+# count = 1
+# while count<= 10:
+#     print(count)
+#     count += 1
+    
