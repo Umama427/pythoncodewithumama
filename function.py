97a88dcb299bac68aacceaed603728bd0722cdd1
Umama khan <umamakhan427@gmail.com>
2024-10-20 06:42:46 +0500

    #DAY:14
    I’ve been working on enhancing my Python skills and recently implemented functions to perform mathematical comparisons and calculations.
    calculateGmean(a, b): This function calculates the geometric mean of two numbers, providing insights into their relationship.
    isGreater(a, b): This function compares two numbers and indicates which one is greater.
    I also created a placeholder function for future development. Here’s a quick example of how I applied these functions to pairs of numbers, showcasing my ability to work with basic algorithms in Python.

---
+++ function.py
@@ -0,0 +1,27 @@
+def calculateGmean(a , b):
+  mean = (a*b)/(a+b)
+  print(mean)
+def isGreater(a,b):
+  if a>b:
+    print("First number is greater")
+  else:
+    print("Second number is greater or equal")
+  # calculateGmean(a,b)
+
+def islesser(a,b):
+  pass # mtlb ye code bd main likhoungi 
+
+
+a = 9
+b = 8
+isGreater(a,b)
+calculateGmean(a,b)
+# gmean = (a*b)/(a+b)
+# print(gmean)
+  
+c = 8
+d = 7
+isGreater(c,d)
+calculateGmean(c,d)
+# gmean1 = (c*d)/(c+d)
+# print(gmean1)
