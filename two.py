00ecf2edad5518006d657784fa8fb9abcdc4358d
Umama khan <umamakhan427@gmail.com>
2024-10-26 01:00:15 +0500

    #DAY:20
    #CodeDescription:
    This function, age_calculator, computes a person's age by subtracting their birth year from the current year. It then prints the calculated age.

---
+++ two.py
@@ -0,0 +1,5 @@
+#person age calculator
+def age_calculator(current_year,birth_year):
+    age= current_year-birth_year
+    print("The age of the person in year is",age,"years")
+age_calculator(2024,2001)
